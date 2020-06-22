f3b9de077671caca87fd5da8ba846a3da3f0a376
Dmitry Spiridonov <yiimar@yandex.ru>
2020-06-22 12:54:07 +0300

    init

---
+++ web/index.php
@@ -0,0 +1,13 @@
+<?php
+
+$config = __DIR__.'/../protected/config/main.php';
+$vendor = __DIR__.'/../vendor/autoload.php';
+
+require_once($vendor);
+
+// remove the following lines when in production mode
+defined('YII_DEBUG') or define('YII_DEBUG',true);
+// specify how many levels of call stack should be shown in each log message
+defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
+
+Yii::createWebApplication($config)->run();
